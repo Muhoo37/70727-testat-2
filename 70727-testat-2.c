20e898e159a843c17b81973962c3ad0bc50f235b
Lucas Noki <lucas-noki@gmx.de>
2020-05-29 15:15:47 +0200

    fix code

--- 70727-testat-2.c
+++ 70727-testat-2.c
@@ -8,72 +8,70 @@
 
 #include <stdio.h>
 
-int main () {
+int main() {
     
-    double bruttogehalt ;
-    int verheiratet , kinder ;
+    double bruttogehalt;
+    int verheiratet, kinder;
     scanf("%lf%d%d", &bruttogehalt, &verheiratet, &kinder);
     
-    double Kindersteuer, Abzuege, Steuersatz, Heiratstatus, abStr;
-   
-   const double steuersatz1 = 0.12;
-   const double steuersatz2 = 0.15;
-   const double steuersatz3 = 0.20;
-   const double steuersatz4 = 0.25;
+    double kinderSteuer, abzuege, steuersatz, heiratsSteuer, abStr;
     
-   
+    const double steuersatz1 = 0.12;
+    const double steuersatz2 = 0.15;
+    const double steuersatz3 = 0.20;
+    const double steuersatz4 = 0.25;
     
-    
-    //Steuersatz nach BruttoGehalt
-     if (bruttogehalt<=12000) {
-        Steuersatz = steuersatz1;
-    } if (bruttogehalt>12000 && bruttogehalt <=20000) {
-        Steuersatz = steuersatz2;
-    } if (bruttogehalt>20000 && bruttogehalt <=30000) {
-        Steuersatz = steuersatz3;
-    } if (bruttogehalt>30000) {
-        Steuersatz = steuersatz4;
-    } if (bruttogehalt<0) {
+    //steuersatz nach BruttoGehalt
+    if (bruttogehalt <= 12000)
+    {
+        steuersatz = steuersatz1;
+    } else if (bruttogehalt > 12000 && bruttogehalt <= 20000)
+    {
+        steuersatz = steuersatz2;
+    } else if (bruttogehalt > 20000 && bruttogehalt <= 30000)
+    {
+        steuersatz = steuersatz3;
+    } else if (bruttogehalt > 30000)
+    {
+        steuersatz = steuersatz4;
+    } else
+    {
         printf("Eingabefehler\n");
         return 0;
     }
-   
-        
+    
     //Verheiratet oder ledig
     if (verheiratet == 1) {
-       Heiratstatus=0.2;
-    if (verheiratet == 0)
-        Heiratstatus = 0;
-    } else {
+        heiratsSteuer = 0.2;
+    } else if (verheiratet == 0)
+    {
+        heiratsSteuer = 0;
+    } else
+        {
         printf("Eingabefehler\n");
-        return 0;
+        return 1;
     }
-   
-   
-    // Kinder
     
-    if(kinder >0) {
-    Kindersteuer = kinder * 0.1;
-    }   else {
+    // Kinder Steuer berechnen, wenn wert kleiner als 0 eingegeben wird return 1 (Error)
+    if (kinder >= 0)
+    {
+        kinderSteuer = kinder * 0.1;
+    } else
+        {
         printf("Eingabefehler\n");
-        return 0;
+        return 1;
     }
     
+    abzuege = 1.0 - heiratsSteuer - kinderSteuer;
     
-    Abzuege = 1.0 - Heiratstatus - Kindersteuer;
-    
-    if(Abzuege >0) {
-        abStr = (bruttogehalt * Steuersatz) * Abzuege;
-    } if (Abzuege <=0) {
-        abStr = (bruttogehalt * Steuersatz);
-    }
-    
-    
-    
+    if (abzuege > 0) {
+        abStr = (bruttogehalt * steuersatz) * abzuege;
+    } else
+        {
+            abStr = (bruttogehalt * steuersatz);
+        }
     printf("%.2lf\n", abStr);
     
-
     return 0;
 }
-
 
